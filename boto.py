f0707acd0872b5e73f7d8781255c0a8a33e4aa4b
TalLicht <tallicht89@gmail.com>
2018-01-16 16:34:45 +0200

    added random joke from API

--- boto.py
+++ boto.py
@@ -4,6 +4,7 @@ This is the template server side for ChatBot
 from sys import argv
 from bottle import route, run, template, static_file, request
 import json
+import requests
 
 
 @route('/', method='GET')
@@ -14,21 +15,19 @@ def index():
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
-    user_message_to_list = user_message.lower()
-    user_message_to_list = user_message_to_list.split()
-    list_of_swears = ['ass','fuck','shit','shitty','hell']
-    if any(word in user_message_to_list for word in list_of_swears):
-        print("first if")
-        a = handle_swears(user_message)
-    else:
-        print("else")
-        a = handle_conversation(user_message)
+    user_message_to_list = user_message.lower().split()
+    for word in user_message_to_list:
+        if word == "joke":
+            print "in joke"
+            a = get_random_joke()
+        else:
+            a = handle_conversation(user_message)
     return json.dumps(a)
 
 
 @route("/test", method='POST')
 def chat():
-    user_message = request.POST.get('msg')
+    user_message = request.get('msg')
     return json.dumps({"animation": "inlove", "msg": user_message})
 
 
@@ -48,12 +47,20 @@ def images(filename):
 
 
 
-def handle_swears(user_message):
-    return {"animation": "no", "msg": "Please don't swear"}
+def get_random_joke():
+    joke = requests.get('http://api.icndb.com/jokes/random')
+    joke = joke.json()
+    joke = joke['value']['joke']
+    return {"animation": "laughing", "msg": joke}
 
 
 def handle_conversation(user_message):
-    if user_message.startswith("My name") or user_message.startswith("my name"):
+    user_message_to_list = user_message.lower()
+    user_message_to_list = user_message_to_list.split()
+    list_of_swears = ['ass', 'fuck', 'shit', 'shitty', 'hell']
+    if any(word in user_message_to_list for word in list_of_swears):
+        return {"animation": "no", "msg": "Please don't swear"}
+    elif user_message.startswith("My name") or user_message.startswith("my name"):
         return {"animation": "inlove", "msg": "Hi"}
     elif user_message.startswith("music"):
         return {"animation": "dancing", "msg": "I love music!"}
@@ -71,6 +78,7 @@ def main(host="0.0.0.0", port=None):
         port = argv[1]   
     run(host=host, port=port)
 
+
 if __name__ == '__main__':
-    # main(host="localhost", port=7000)  # run on localhost
-    main()
+    main(host="localhost", port=7000)  # run on localhost
+    #main()
