e6d91d6307f22206327d3fca5dbc7f515540e003
TalLicht <tallicht89@gmail.com>
2018-01-18 13:47:26 +0200

    added date and time functions

--- boto.py
+++ boto.py
@@ -3,8 +3,7 @@ This is the template server side for ChatBot
 """
 from sys import argv
 from bottle import route, run, template, static_file, request
-import json
-import requests
+import datetime , time, json, requests
 
 
 @route('/', method='GET')
@@ -15,14 +14,23 @@ def index():
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
+    if user_message[-1] =="?" or user_message[-1] =="!" or user_message[-1] ==":" or user_message[-1] =="," or user_message[-1] ==".":
+         user_message = user_message[:-1]
     user_message_to_list = user_message.lower().split()
     for word in user_message_to_list:
+        # if word == "name":
+
         if word == "joke":
             return json.dumps(get_random_joke())
         elif word == "weather":
             return json.dumps(get_weather(user_message))
+        elif word == "time":
+            return json.dumps(get_time())
+        elif word == "date":
+            return json.dumps(get_date())
         else:
-            return json.dumps(handle_conversation(user_message))
+            respond = handle_conversation(user_message)
+    return json.dumps(respond)
 
 
 @route("/test", method='POST')
@@ -50,23 +58,30 @@ def get_random_joke():
     joke = requests.get('http://api.icndb.com/jokes/random')
     joke = joke.json()
     joke = joke['value']['joke']
-    return {"animation": "laughing", "msg": joke}
+    return {"animation": "giggling", "msg": joke}
 
 
 def get_weather(user_message):
-    user_message_edited = user_message.lower().split("in")
-    city_name = user_message_edited[1].replace("?","") [1:]
-    print len(city_name)
+    user_message = user_message.lower().split("in")
+    city_name = user_message[1][1:]
     weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ city_name + '&appid=5d1452edf89427c2b801a97919ea0a8b')
     json_object = weather.json()
     temp_k = int(json_object['main']['temp'])
     temp_c = int(temp_k - 273.15)
     if temp_c < 15:
-        return {"animation": "takeoff", "msg": "The weather in " + str(city_name) + " is " + str(temp_c) + " celsius. So cold!"}
+        return {"animation": "takeoff", "msg": "The weather in " + str(city_name.capitalize()) + " is " + str(temp_c) + " celsius. So cold!!"}
     elif 16 < temp_c < 28:
-        return {"animation": "inlove", "msg": "The weather in " + str(city_name) + " is " + str(temp_c) + " celsius. Nice and warm, just as I love it!"}
+        return {"animation": "inlove", "msg": "The weather in " + str(city_name.capitalize()) + " is " + str(temp_c) + " celsius. Nice and warm, just as I love it!"}
     else:
-        return {"animation": "takeoff", "msg": "The weather in " + str(city_name) + " is " + str(temp_c) + " celsius. Way too hot for me.."}
+        return {"animation": "takeoff", "msg": "The weather in " + str(city_name.capitalize()) + " is " + str(temp_c) + " celsius. Way too hot for me.."}
+
+
+def get_time():
+    return {"animation": "waiting", "msg": "The time right now is " + str(time.strftime("%H:%M"))}
+
+
+def get_date():
+    return {"animation": "waiting", "msg": "Today is " + datetime.date.today().strftime("%B") + " " + time.strftime("%d") + " " + time.strftime("%Y")}
 
 
 def handle_conversation(user_message):
